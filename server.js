14734ce888c29b73ac514aa58ef1a0e3ae473977
Enrico Alvarenga <enricomalvarenga@gmail.com>
2016-08-02 18:09:38 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const PORT = process.env.port || 3000;
+const express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function () {
+  console.log('Server launched!');
+})
